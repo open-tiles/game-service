584354bf9bbcf9273ef60c6deb9b1998f5fa4455
Adam Blackwater <me@adamblackwater.com>
2020-05-22 20:17:57 +0100

    Add test attack

--- models/player.py
+++ models/player.py
@@ -1,5 +1,20 @@
+from aiohttp import web
+
+
 class Player:
 
-    def __init__(self, pid, name):
+    def __init__(self, pid, name, tiles):
         self.pid = pid
         self.name = name
+        self.tiles = tiles
+
+    async def attack(request):
+        data = await request.json()
+        territories = {
+                'France': 11,
+                'UK': 20
+                }
+        at = territories[data['attacker']]
+        dt = territories[data['defender']]
+        print(at - dt)
+        return web.json_response()
