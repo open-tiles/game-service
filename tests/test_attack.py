31dd22cb0024447c317787678c8df87e9a7807b5
Adam Blackwater <me@adamblackwater.com>
2020-07-18 12:48:02 +0100

    Add tests

--- tests/test_attack.py
+++ tests/test_attack.py
@@ -1,1 +1,27 @@
+import json
 import attack
+
+
+class FakeURL():
+
+    def __init__(self, queries={}):
+        self.query = queries
+
+
+class FakeRequest():
+
+    def __init__(self, _raise_exception=False, _json=None, app=None, url=None):
+        self._raise_exception = _raise_exception
+        self._json = _json
+        self.app = app or {}
+        self.url = url
+
+    async def json(self):
+        if self._raise_exception:
+            json.loads('None')
+        return self._json
+
+
+async def test_attack():
+    request = FakeRequest()
+    await attack.hex_attack(request)
--- tests/test_map.py
+++ tests/test_map.py
@@ -1,7 +1,3 @@
-class FakeRequest():
-
-    def __init(self, data=None):
-        self.data = data
 
 
 async def test_get_baorders():
